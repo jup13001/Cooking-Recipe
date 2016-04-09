346df4a1122ea3a8e4857e696a0c3180f937b750
SqgTang <sqg.tang@gmail.com>
2016-04-09 17:40:23 -0400

    Added files via upload

--- Stopwatch.java
+++ Stopwatch.java
@@ -1,42 +1,43 @@
+import java.util.Scanner;
+import java.math.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Stopwatch 
+{
+	static int interval;
+	static Timer timer;
 
-	import java.util.Scanner;
-	import java.util.Timer;
-	import java.util.TimerTask;
-
-	public class Stopwatch 
+	public static void main(String[] args) 
 	{
-		static int interval;
-		static Timer timer;
-
-		public static void main(String[] args) 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input seconds => : ");
+		String secs = sc.nextLine();
+		int delay = 1000;
+		int period = 1000;
+		timer = new Timer();
+		interval = Integer.parseInt(secs);
+		System.out.println(secs);
+		timer.scheduleAtFixedRate(new TimerTask() 
 		{
-			Scanner sc = new Scanner(System.in);
-			System.out.print("Input seconds => : ");
-			String secs = sc.nextLine();
-			int delay = 1000;
-			int period = 1000;
-			timer = new Timer();
-			interval = Integer.parseInt(secs);
-			System.out.println(secs);
-			timer.scheduleAtFixedRate(new TimerTask() 
+			public void run() 
 			{
+				System.out.println(setInterval());
 
-				public void run() 
-				{
-					System.out.println(setInterval());
-
-				}
-			}, delay, period);
-		}
-
-		private static final int setInterval() 
-		{
-			if (interval == 1)
-				timer.cancel();
-			return --interval;
-		}
+			}
+		}, delay, period);
 	}
 
-
-
+	private static final String setInterval() 
+	{
+		if (interval == 1)
+			timer.cancel();
+		int hours = Math.floorDiv(interval, 3600);
+		int remainders = interval % 3600;
+		int minutes = Math.floorDiv(remainders, 60);
+		remainders = remainders % 60;
+		
+		--interval;
+		return "" + hours + " hours, " + minutes + " minutes, " + remainders + " seconds";
+	}
+}
